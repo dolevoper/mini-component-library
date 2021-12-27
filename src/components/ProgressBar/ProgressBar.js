bc7975dca94b2165914529bfd8bb26f51f2ec4b8
Omer Dolev <omerdolev90@gmail.com>
2021-12-27 12:56:19 +0200

    create progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,65 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const sizeVariables = {
+  small: {
+    height: "8px",
+    borderRadius: "4px",
+    padding: "0"
+  },
+  medium: {
+    height: "12px",
+    borderRadius: "4px",
+    padding: "0"
+  },
+  large: {
+    height: "24px",
+    borderRadius: "8px",
+    padding: "4px"
+  }
 };
+
+const ProgressBar = ({ value, size }) => {
+  const vars = sizeVariables[size];
+
+  if (!vars) throw new Error(`Invalid 'size' prop for ProgressBar: ${size}`);
+
+  return (
+    <Wrapper aria-valuenow={value} style={{
+      "--padding": vars.padding,
+      "--borderRadius": vars.borderRadius
+    }}>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <ProgressWrapper>
+        <ProgressIndicator style={{
+          "--height": vars.height,
+          "--width": `${value}%`
+        }} />
+      </ProgressWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div.attrs({
+  role: "progressbar",
+  "aria-valuemin": 0,
+  "aria-valuemax": 100,
+})`
+background-color: ${COLORS.transparentGray15};
+border-radius: var(--borderRadius);
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+padding: var(--padding);
+`;
+
+const ProgressWrapper = styled.div`
+border-radius: 4px;
+overflow: hidden;
+`;
+
+const ProgressIndicator = styled.div`
+height: var(--height);
+width: var(--width);
+background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
