a7701816947f6156b29cad2614563652a3116950
Omer Dolev <omerdolev90@gmail.com>
2022-11-10 14:51:37 +0200

    icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,23 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const vars = {
+  small: {
+    "--font-size": `${14 / 16}rem`,
+    "--border-width": "1px",
+    "--padding-left": "24px",
+    "--height": `${24 / 16}rem`,
+    "--icon-top": "4px"
+  },
+  large: {
+    "--font-size": `${18 / 16}rem`,
+    "--border-width": "2px",
+    "--padding-left": "36px",
+    "--height": `${36 / 16}rem`,
+    "--icon-top": "6px"
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +30,45 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper style={vars[size]}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconContainer><Icon id={icon} size={size === "large" ? 24 : 16} /></IconContainer>
+      <Input placeholder={placeholder} style={{ "--width": `${width}px` }} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+display: block;
+position: relative;
+color: ${COLORS.gray700};
+
+&:hover {
+  color: ${COLORS.black};
+}
+`;
+
+const Input = styled.input`
+border: none;
+border-bottom: var(--border-width) solid ${COLORS.black};
+outline-offset: 2px;
+padding-left: var(--padding-left);
+height: var(--height);
+width: var(--width);
+font-size: var(--font-size);
+font-weight: 700;
+color: inherit;
+
+&::placeholder {
+  font-weight: 400;
+  color: ${COLORS.gray500};
+}
+`;
+
+const IconContainer = styled.span`
+position: absolute;
+top: var(--icon-top)
+`;
 
 export default IconInput;
